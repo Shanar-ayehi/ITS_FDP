6c9dd2e47025ab9277dbb50b2eb53c095bd13913
Francesco Scuderi <frascuderi05@gmail.com>
2024-12-20 16:09:21 +0000

    Hello .c updated

--- Hello/Hello.C
+++ Hello/Hello.C
@@ -2,7 +2,19 @@
  
 int main ()
 {
-    printf("Hello World");
+    printf("Hello World\n");
+
+    // variable of 32 chars
+    char name[32];
+
+    // ask for a name
+    printf("Enter a name\n");
+
+    // read the input of the user
+    scanf("%s", name);
+
+    // print a message with the name entered
+    printf("\nHello \t%s !\n\n\n", name);
 
     return 0;
 }
