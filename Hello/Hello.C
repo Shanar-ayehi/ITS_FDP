734edcda24581db196879dfbb11184835897b9f7
Francesco Scuderi <frascuderi05@gmail.com>
2025-01-02 14:51:35 +0000

    Compito finito

--- Hello/Hello.C
+++ Hello/Hello.C
@@ -2,19 +2,24 @@
  
 int main ()
 {
-    printf("Hello World\n");
-
     // variable of 32 chars
-    char name[32];
+    char nome[32];
 
     // ask for a name
-    printf("Enter a name\n");
+    printf("Inserisci il tuo nome:\n");
+    // scan user input
+    scanf("%s", nome);
+    
+    char cognome[32];
+
+    //ask for a surname
+    printf("Inserisci il tuo cognome:\n");
 
     // read the input of the user
-    scanf("%s", name);
+    scanf("%s", cognome);
 
-    // print a message with the name entered
-    printf("\nHello \t%s !\n\n\n", name);
+    // print a message with the user input and the thanks
+    printf("\nBenvenuto, %s %s !\nTi ringraziamo per aver usato il nostro programma.\nTi auguriamo buone feste \n\t    *\n\t   ***\n\t  *****\n\t *******\n\t   ***\n", nome, cognome);
 
     return 0;
 }
